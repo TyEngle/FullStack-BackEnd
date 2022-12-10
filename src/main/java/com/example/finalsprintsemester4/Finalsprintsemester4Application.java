52ca475eb03c77d0b2546c8367509b8c7a7e31e3
NoahG43 <95226957+NoahG43@users.noreply.github.com>
2022-12-10 14:16:23 -0330

    Initial commit

---
+++ src/main/java/com/example/finalsprintsemester4/Finalsprintsemester4Application.java
@@ -0,0 +1,13 @@
+package com.example.finalsprintsemester4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalsprintsemester4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalsprintsemester4Application.class, args);
+	}
+
+}
