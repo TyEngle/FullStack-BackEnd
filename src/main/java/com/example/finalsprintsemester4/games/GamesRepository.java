8e3de8fca1942d1580a56455fcf25a088696ce0a
NoahG43 <95226957+NoahG43@users.noreply.github.com>
2022-12-11 14:03:23 -0330

    Completed Games API aside from database credentials

---
+++ src/main/java/com/example/finalsprintsemester4/games/GamesRepository.java
@@ -0,0 +1,11 @@
+package com.example.finalsprintsemester4.games;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GamesRepository extends JpaRepository<Games, Long> {
+
+}
